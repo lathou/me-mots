ad0ceba0f2660e6542c0fa03d312167f0a4bfc5b
Nath <tholfou@gmail.com>
2015-12-26 13:37:13 +0100

    le bouton passe compte comme réponse fausse

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,19 @@ var mot,
 	bonneReponse,
 	ok = 0,
 	ko = 0;
+
+var mots = [{mot: 'soleil', traduction: 'sun'}, {mot: 'chocolat', traduction: 'chocolate'}, 
+	{mot: 'singe', traduction: 'monkey'}, {mot: 'fauteuil', traduction: 'sofa'}, {mot: 'stylo', traduction: 'pen'}, 
+	{mot: 'lit', traduction: 'bed'}, {mot: 'porte', traduction: 'door'}];
+
+var form = document.getElementById('test');
 var inputReponse = document.getElementById('inputReponse');
 var btnValider = document.getElementById('valider');
 var btnPasse = document.getElementById('passe');
 var correction = document.getElementById('correction');
-var form = document.getElementById('test');
 var compteurOk = document.getElementById('compteurOk');
 var compteurKo = document.getElementById('compteurKo');
 var progressBar = document.getElementById('progressBar');
-
-miseAJourResultats();
-genererMot();
 
 document.addEventListener('keydown',function(e){
 	if(e.keyCode === 13){
@@ -21,6 +23,9 @@ document.addEventListener('keydown',function(e){
 	}
 }, false);
 
+miseAJourResultats();
+genererMot();
+
 
 /*******************-Fonctions-***************************/
 
@@ -28,33 +33,28 @@ document.addEventListener('keydown',function(e){
 function init(){
 	inputReponse.addEventListener('keyup', griserBoutonValider, false);
 	btnValider.addEventListener('click', affichageCorrection, false);
-	btnPasse.addEventListener('click', affichageCorrection, false);
+	btnPasse.addEventListener('click', afficheFaux, false);
 
-	correction.className='col-md-1';
-	inputReponse.value = '';
-	inputReponse.focus();
-	inputReponse.style.color = 'rgb(85, 85, 85)';
-	btnValider.disabled = true;
-	inputReponse.disabled = false;
 	form.className = 'jumbotron text-center';
+	correction.className='col-md-1';
+	inputReponse.value = '';	
+	inputReponse.style.color = '#555555';
+	inputReponse.disabled = false;
+	inputReponse.focus();
+	btnValider.disabled = true;
+	btnPasse.disabled = false;	
 }
 
 function griserBoutonValider(){
 	if(inputReponse.value.length){
-		btnValider.className = 'btn btn-default';
 		btnValider.disabled = false;
 	}else{
-		btnValider.className = 'btn btn-default disabled';
 		btnValider.disabled = true;
 	}
 }
 
 function genererMot(){	
 	init();
-
-	var mots = [{mot: 'soleil', traduction: 'sun'}, {mot: 'chocolat', traduction: 'chocolate'}, 
-	{mot: 'singe', traduction: 'monkey'}, {mot: 'fauteuil', traduction: 'sofa'}, {mot: 'stylo', traduction: 'pen'}, 
-	{mot: 'lit', traduction: 'bed'}, {mot: 'porte', traduction: 'door'}];
 	var ancienMot = mot;
 	while(mot === ancienMot || !mot){
 		mot = mots[Math.floor(Math.random()*mots.length)].mot;
@@ -65,45 +65,50 @@ function genererMot(){
 }
 
 function affichageCorrection(){
-		inputReponse.removeEventListener('keyup', griserBoutonValider, false);
-		btnValider.removeEventListener('click', affichageCorrection, false);
-		btnPasse.removeEventListener('click', affichageCorrection, false);
-		inputReponse.disabled = "disabled";
-
-		if(verifierMot(mot, inputReponse.value)){		
-			correction.className='col-md-1 glyphicon glyphicon-ok';
-			form.className = 'jumbotron text-center has-success';
-			ok++;
-			setTimeout(genererMot, 1000);
-			
+		bonneReponse = reponseExact();
+		if(bonneReponse.toLowerCase() === inputReponse.value.toLowerCase()){		
+			afficheVrai();			
 		}else{
-			correction.className='col-md-1 glyphicon glyphicon-remove';
-			inputReponse.value = bonneReponse;
-			inputReponse.style.color = 'red';
-			btnValider.className = 'btn btn-default disabled';
-			form.className = 'jumbotron text-center has-error';
-			ko++;
-			setTimeout(genererMot, 1000);			
-		}
+			afficheFaux();
+		}	
+}
+
+function afficheVrai(){
+	desactiverBoutonEtInput();
+	correction.className='col-md-1 glyphicon glyphicon-ok';
+	form.className = 'jumbotron text-center has-success';
+	ok++;
+	setTimeout(genererMot, 1000);
 	miseAJourResultats();
 }
 
-function verifierMot(mot, saisie){
-	var mots = [{mot: 'soleil', traduction: 'sun'}, {mot: 'chocolat', traduction: 'chocolate'}, 
-	{mot: 'singe', traduction: 'monkey'}, {mot: 'fauteuil', traduction: 'sofa'}, {mot: 'stylo', traduction: 'pen'}, 
-	{mot: 'lit', traduction: 'bed'}, {mot: 'porte', traduction: 'door'}];
-	var verifie = false;
+function afficheFaux(){
+	desactiverBoutonEtInput();	
+	correction.className='col-md-1 glyphicon glyphicon-remove';
+	inputReponse.value = reponseExact();
+	inputReponse.style.color = 'red';
+	form.className = 'jumbotron text-center has-error';
+	ko++;
+	setTimeout(genererMot, 1000);
+	miseAJourResultats();	
+}
+		
+function desactiverBoutonEtInput(){
+	btnValider.removeEventListener('click', affichageCorrection, false);
+	btnPasse.removeEventListener('click', affichageCorrection, false);
+	inputReponse.disabled = true;
+	btnValider.disabled = true;
+	btnPasse.disabled = true;
+}
 
+
+function reponseExact(){
 	mots.forEach(function(value, id, array){
 		if (mot === mots[id].mot){
-			if(saisie.toLowerCase() === mots[id].traduction.toLowerCase()){
-				verifie = true;
-			}else{
-				bonneReponse = mots[id].traduction;
-			}
+			bonneReponse = mots[id].traduction;
 		}
 	});
-	return verifie;	
+	return bonneReponse;	
 }
 
 function miseAJourResultats(){
